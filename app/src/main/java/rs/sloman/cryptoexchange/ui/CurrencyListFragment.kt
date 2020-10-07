55b501643cec03cdcdc49258e910bb8d31767d82
sLomanojlo <slobodanmanojlovic@gmail.com>
2020-10-07 19:46:54 +0200

    CurrencyListFragment and Navigations basics

---
+++ app/src/main/java/rs/sloman/cryptoexchange/ui/CurrencyListFragment.kt
@@ -0,0 +1,8 @@
+package rs.sloman.cryptoexchange.ui
+
+import androidx.fragment.app.Fragment
+import rs.sloman.cryptoexchange.R
+
+
+class CurrencyListFragment : Fragment(R.layout.fragment_currency_list) {
+}
